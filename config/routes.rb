36f06b96c4b3500885592d360fd55af738139435
Daniel Harris <bobth3bum@gmail.com>
2015-01-26 16:32:09 -0700

    tasks page working with bootstrap up until date for tasks utilization

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get 'faq' => 'commonquestions#faq'
   get 'tasks' => 'welcome#tasks'
   get 'index' => 'welcome#index'
+  get 'show' => 'tasks#show'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
