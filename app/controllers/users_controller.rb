462a479df63e5537a43949b8d7feb11074ebf623
Daniel Harris <bobth3bum@gmail.com>
2015-02-05 09:28:41 -0700

    fixed update page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,9 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to users_path, :notice => "User was successfully updated."
   end
 
   def user_params
