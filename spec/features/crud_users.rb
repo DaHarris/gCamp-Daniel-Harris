23474a88f74bec802d207b3ab58e53ee95ad60b3
Daniel Harris <bobth3bum@gmail.com>
2015-02-18 16:37:07 -0700

    add capybara specs for users

--- spec/features/crud_users.rb
+++ spec/features/crud_users.rb
@@ -11,30 +11,40 @@ describe 'User can CRUD Events' do
     click_on "New User"
 
     #filling out form for location
-    fill_in 'user[first_name]', :with => "Dan"
-    fill_in 'user[last_name]', :with => "Harris"
-    fill_in 'user[email]', :with => "bobth3bum@gmail.com"
-    fill_in 'user[password]', with: "my_pass"
-    fill_in 'user[password_confirmation]', with: "my_pass"
+    fill_in "First name", with: "Dan"
+    fill_in "Last name", with: "Harris"
+    fill_in "Email", with: "bobth3bum@gmail.com"
+    fill_in "Password", with: "my_pass"
+    fill_in "Password confirmation", with: "my_pass"
 
     click_on "Create User"
-
-    click_on "Sign in"
-
-    fill_in 'email', :with => "bobth3bum@gmail.com"
-    fill_in 'password', :with => "my_pass"
-
-    click_on "Signin"
-    
-
-    #submitting form to create a location
-    click_on "Create Location"
-
-    click_on 'Add Event'
-
-    fill_in 'event[description]', :with => 'Dinner Party'
-    fill_in 'event[date]', :with => '01/01/2015'
-    check 'event[requires_id]'
-
-    click_on 'Create Event'
   end
+
+  it 'Can create user' do
+    expect(page).to have_content("Dan")
+  end
+
+  it 'Can edit user' do
+    click_on "Edit"
+
+    fill_in "First name", with: "Daniel"
+    fill_in "Password", with: "my_pass"
+    fill_in "Password confirmation", with: "my_pass"
+
+    click_on "Update User"
+
+    expect(page).to have_content("Daniel")
+  end
+
+  it 'Can view user show page' do
+    click_on "Dan"
+
+    expect(page).to have_content("First Name")
+  end
+
+  it 'Can delete user' do
+    click_on "Delete"
+
+    expect(page).to have_no_content("Dan")
+  end
+end
