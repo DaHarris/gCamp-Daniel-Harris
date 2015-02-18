e69249af6a06a654467dd98edba17da88cde3752
Daniel Harris <bobth3bum@gmail.com>
2015-02-18 14:04:49 -0700

    add validations and confirmations for password user model and new/edit form for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :first_name, :last_name, :email, :password, presence: true
   validates :email, uniqueness: true
+  validates :password, confirmation: true
 
 end
