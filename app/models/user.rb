462b24772e2d8dbcd4d30b1d57e6ecdae20f56ab
Daniel Harris <bobth3bum@gmail.com>
2015-02-18 13:57:51 -0700

    add bcrypt and has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  validates :first_name, :last_name, :email, presence: true
+  has_secure_password
+  validates :first_name, :last_name, :email, :password, presence: true
   validates :email, uniqueness: true
 
 end
