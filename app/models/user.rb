cfadb2edfcb56ec53c543f65e3e95b3e4d5763ae
Daniel Harris <bobth3bum@gmail.com>
2015-03-09 14:26:54 -0600

    add model relations for membership join table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
+  has_many :memberships
+  has_many :projects, through: :memberships
 
 end
