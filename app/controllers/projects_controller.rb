fe4c44300ebcbe8f0e2cdf6aae9d63d658cb21cf
Daniel Harris <bobth3bum@gmail.com>
2015-04-07 14:50:47 -0600

    project creator instantly becomes project owner / owner membership

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,6 +16,7 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
     if @project.save
+      Membership.create(owner: true, user_id: current_user.id, project_id: @project.id)
       redirect_to project_path(@project), :notice => 'Project was successfully created.'
     else
       @messages = @project.errors.full_messages
