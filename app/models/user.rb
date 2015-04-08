7213b64b16d9d08db2404f4c0b50e183cc10621d
Daniel Harris <bobth3bum@gmail.com>
2015-04-08 14:56:07 -0600

    only projects members can see eachothers email addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,17 @@ class User < ActiveRecord::Base
   has_many :projects, through: :memberships
   has_many :comments, dependent: :nullify
 
+  def project_member(user)
+    self.projects.each do |pro|
+      pro.memberships.each do |x|
+        if user.id == x.user_id
+          return true
+        end
+      end
+    end
+    return false
+  end
+
   def full_name
     "#{first_name} #{last_name}"
   end
