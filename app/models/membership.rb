b7a42e421b42b014a72b0ec0212d0df0ebc97589
Daniel Harris <bobth3bum@gmail.com>
2015-04-08 13:19:56 -0600

    change validations for uniqueness on membership model

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,6 +2,14 @@ class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
   validates :user_id, presence: true
-  validates :user_id, uniqueness: {message: " has already been added to this project"}
+  validates :project_id, presence: true
+  validate :check_unique
 
+  private
+
+  def check_unique
+    if Membership.find_by(user_id: user_id, project_id: project_id)
+      errors.add(:base, "User has already been added to this project")
+    end
+  end
 end
