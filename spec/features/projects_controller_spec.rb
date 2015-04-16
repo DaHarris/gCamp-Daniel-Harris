7f6a7fffedcdce516a79fe9830fabf3d10134b95
Daniel Harris <bobth3bum@gmail.com>
2015-04-16 14:51:25 -0600

    projects controller specs

--- spec/features/projects_controller_spec.rb
+++ spec/features/projects_controller_spec.rb
@@ -1,3 +1,34 @@
 require 'rails_helper'
+require 'factory'
 
-describe ''
+RSpec.describe ProjectsController, type: :controller do
+
+  before :each do
+    @user = FactoryGirl.create(:user)
+    session[:user_id] = @user.id
+    @project = FactoryGirl.create(:project)
+  end
+
+  it "Creates a project with #Post" do
+    project_test = Project.first
+    expect(project_test.name).to eq("project test")
+  end
+
+  it "Updates a project with #Patch" do
+    put :update, {
+      :id => @project.id,
+      :project => {
+        name: "updated"
+      }
+    }
+    @project.reload
+    expect(@project.name).to eq("updated")
+  end
+
+  it 'Deletes a project' do
+    total = Project.all.count
+    delete :destroy, :id => @project.id
+    expect(Project.all.count).to eq(total-1)
+  end
+
+end
